a815dbc3d8880833d3c1b81a697a4aee4ba7a81d
andrey8910 <a.ivanov6444@gmail.com>
2022-08-11 11:02:02 +0300

    fix styles header responsive & add scripts for burger-menu & add .gitignore

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//Получаем бургер
+const headerBurger = document.querySelector(".header__burger");
+const headerMenu = document.querySelector(".header__menu");
+
+//Вешаем слушатель событий при его клике - вызываем функцию.
+headerBurger.addEventListener("click", function() {
+    //Функция добавляет класс или удаляет его.
+    headerBurger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+});
