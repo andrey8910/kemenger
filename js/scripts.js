dd505bc8668ae1370f463dc53b54cd36f55e9bca
ivanov6444 <a.ivanov6444@gmail.com>
2022-08-16 16:58:29 +0300

    fix catalog slider

--- js/scripts.js
+++ js/scripts.js
@@ -8,34 +8,48 @@ headerBurger.addEventListener("click", function() {
     headerBurger.classList.toggle('active');
     headerMenu.classList.toggle('active');
 });
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+let btn = document.querySelectorAll(".hidden_btn");
+console.log(btn)
+btn.forEach((element) => {
+
+    element.addEventListener("click", e =>{
+            console.log(e);
+
+    })
+
+});
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Catalog read more button
- const slide1_btn = document.getElementById('item1');
- const slide1 = document.getElementById(slide1_btn.id + "__slide");
-  slide1_btn.addEventListener("click", function (){
-      console.log(slide1.style)
-      if (slide1.style.display === "" || slide1.style.display === "none" ) {
-          slide1_btn.innerHTML = "скрыть";
-          slide1.style.display = "inline";
-      }else {
-          slide1_btn.innerHTML = "Подробнее ..."
-          slide1.style.display = "none";
-      }
-
-  })
-
- const slide2_btn = document.getElementById('item2');
- const slide2 = document.getElementById(slide2_btn.id + "__slide");
- slide2_btn.addEventListener("click", function (){
-     if (slide2.style.display === "" || slide1.style.display === "none") {
-         slide2_btn.innerHTML = "скрыть";
-         slide2.style.display = "inline";
-     }else {
-         slide2_btn.innerHTML = "Подробнее ...";
-         slide2.style.display = "none";
-     }
-
- })
+//  const slide1_btn = document.getElementById('item1');
+//  const slide1 = document.getElementById(slide1_btn.id + "__slide");
+//   slide1_btn.addEventListener("click", function (){
+//       console.log(slide1.style)
+//       if (slide1.style.display === "" || slide1.style.display === "none" ) {
+//           slide1_btn.innerHTML = "скрыть";
+//           slide1.style.display = "inline";
+//       }else {
+//           slide1_btn.innerHTML = "Подробнее ..."
+//           slide1.style.display = "none";
+//       }
+//
+//   })
+//
+//  const slide2_btn = document.getElementById('item2');
+//  const slide2 = document.getElementById(slide2_btn.id + "__slide");
+//  slide2_btn.addEventListener("click", function (){
+//      if (slide2.style.display === "" || slide1.style.display === "none") {
+//          slide2_btn.innerHTML = "скрыть";
+//          slide2.style.display = "inline";
+//      }else {
+//          slide2_btn.innerHTML = "Подробнее ...";
+//          slide2.style.display = "none";
+//      }
+//
+//  })
 
  // function readMore(el){
  //     let btn = el.attributes.id
